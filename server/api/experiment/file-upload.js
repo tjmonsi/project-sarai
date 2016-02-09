b61f7756612e32a48b6e6a59b83fbd8b83580329
TJ <tjmonsi@gmail.com>
2016-02-09 14:21:34 +0800

    Fixed util using hack

--- server/api/experiment/file-upload.js
+++ server/api/experiment/file-upload.js
@@ -1,71 +1,52 @@
-// import {Picker} from 'meteor/meteorhacks:picker';
-// import {Accounts} from 'meteor/accounts-base';
+import {Picker} from 'meteor/meteorhacks:picker';
+import {Accounts} from 'meteor/accounts-base';
 
-setTimeout(() => {
-  try {  
-    require('multiparty');
-    // console.log(multiparty);
+import util from 'util';
+
+import {Meteor} from 'meteor/meteor';
+import multiparty from 'multiparty';
+
+Picker.route('/file-upload', (params, req, res) => {
+  const userId = req.headers['x-user-id'];
+  const userToken = req.headers['x-user-token'];
+  if (userId && userToken) {
+    // Authenticate userId and userToken here
+    const hashToken = Accounts._hashLoginToken(userToken);
+    console.log(userId);
+    console.log(userToken);
+    console.log(hashToken);
+
+    const form = new multiparty.Form();
+
+    form.on('part', (part) => {
+      // console.log(part.pipe);
+      if (!part.filename) {
+        // console.log('got field name '+part.name);
+        part.resume();
+      }
+
+      if (part.filename) {
+        // console.log('got file name '+part.filename);
+        part.resume();
+      }
+
+      part.on('error', (err) => {
+        console.log(err);
+        // part.resume();
+      });
+    });
+
+    form.on('error', (err) => {
+      console.log(err);
+      // something
+    });
+
+    form.on('close', () => {
+      res.end('Done');
+    });
+
+    form.parse(req);
+  } else {
+    res.end('');
   }
-  catch(e) {
-    console.log(e);
-  }
-}, 1000)
-
-
-// import util from 'util';
-
-// const util = require('util');
-// Meteor.startup(() => {
-    
-
-//   // console.log(util.inherits);  
-// })
-
-
-// import {Meteor} from 'meteor/meteor';
-// import multiparty from 'multiparty';
-
-// Picker.route('/file-upload', (params, req, res) => {
-//   const userId = req.headers['x-user-id'];
-//   const userToken = req.headers['x-user-token'];
-//   if (userId && userToken) {
-//     // Authenticate userId and userToken here
-//     const hashToken = Accounts._hashLoginToken(userToken);
-//     console.log(userId);
-//     console.log(userToken);
-//     console.log(hashToken);
-
-//     const form = new multiparty.Form();
-
-//     form.on('part', (part) => {
-//       // console.log(part.pipe);
-//       if (!part.filename) {
-//         // console.log('got field name '+part.name);
-//         part.resume();
-//       }
-
-//       if (part.filename) {
-//         // console.log('got file name '+part.filename);
-//         part.resume();
-//       }
-
-//       part.on('error', (err) => {
-//         console.log(err);
-//         // part.resume();
-//       });
-//     });
-
-//     form.on('error', (err) => {
-//       console.log(err);
-//       // something
-//     });
-
-//     form.on('close', () => {
-//       res.end('Done');
-//     });
-
-//     form.parse(req);
-//   } else {
-//     res.end('');
-//   }
-// });
+});
