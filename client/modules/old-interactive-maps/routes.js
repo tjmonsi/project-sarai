803e61df070a4ebd11947e1656831c22773e9749
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-02-22 05:19:40 +0000

    Made interactive maps hardcode

--- client/modules/old-interactive-maps/routes.js
+++ client/modules/old-interactive-maps/routes.js
@@ -1,14 +1,33 @@
-// import React from 'react';
-import {Main} from '/client/modules/ui-components-v2';
+import React from 'react';
+import {Main, SectionList} from '/client/modules/ui-components-v2';
+import SuitabilityMap from './components/suitability-maps.jsx';
+import {coreLib, CoreNavList, CoreBanner} from '/client/modules/core-v2';
 
 export default (injectDeps, context) => {
   const {FlowRouter, mount} = context;
+  const {triggerAuthenticate, appBarTitle, appDrawerTitle, coreLayout} = coreLib;
+  const layout = coreLayout('core', appBarTitle, appDrawerTitle, true, CoreNavList);
   const MainCtx = injectDeps(Main);
 
   FlowRouter.route('/interactive-maps', {
     name: 'interactive-maps.root',
+    triggersEnter: [triggerAuthenticate],
     action() {
-      mount(MainCtx, {});
+      mount(MainCtx, Object.assign({}, layout, {
+        content: () => (React.createElement(SectionList, {
+          displayName: 'SectionList',
+          spacing: false,
+          sections: [
+            // () => (React.createElement(CoreBanner, {
+            //   displayName: 'CoreBanner',
+            //   path: this.name
+            // })),
+            () => (React.createElement(SuitabilityMap, {
+              displayName: 'SuitabilityMap'
+            }))
+          ]
+        }))
+      }));
     }
   });
 };
